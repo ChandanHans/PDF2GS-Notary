2b8d7b8e455e37661b4fb458767a25e810cd9007
ChandanHans <chandanhans2003@gmail.com>
2024-04-16 23:54:54 +0530

    requirements

--- main.py
+++ main.py
@@ -6,44 +6,64 @@ import pandas as pd
 from tqdm import tqdm
 from src.pdf_processing import pdf_to_images
 from src.excel_util import save_table
-from src.image_processing import process_image, is_tesseract_installed
+from src.image_processing import process_image
 from src.utils import *
 
 
 def main():
     time_start = time.time()
-    pdf_files = [file for file in os.listdir(INPUT_FOLDER) if file.lower().endswith(".pdf")]
+    pdf_files = [
+        file for file in os.listdir(INPUT_FOLDER) if file.lower().endswith(".pdf")
+    ]
 
     for pdf in pdf_files:
         ptime = time.time()
         print(f"\nProcess Started For {pdf}\n")
-    
+
         pdf_path = f"{INPUT_FOLDER}/{pdf}"
         pdf_to_images(pdf_path, IMAGE_FOLDER, 200)
-        
-        images = [file for file in os.listdir(IMAGE_FOLDER) if file.lower().endswith(".png")]
-        images = sorted(images, key = extract_number)
+
+        images = [
+            file for file in os.listdir(IMAGE_FOLDER) if file.lower().endswith(".png")
+        ]
+        images = sorted(images, key=extract_number)
         data = []
-        
+
         print("\nSTART :\n")
         total_cost = 0
-        for image in tqdm(images, ncols=60, bar_format="{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}"):
+        progress_bar = tqdm(images, ncols=60, bar_format="{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}")
+        for image in progress_bar:
             result, cost = process_image(image)
             total_cost += cost
-            data.append(result)
+            if result:
+                data.append(result)
         print()
-        
+
         df = pd.DataFrame(data)
-        df.columns = ["Name","Data Of Birth","Data Of Death","Declarant","Data Of Notoriety","Notary","Address","Phone","Email","Website","Status"]
-        df = df.replace([pd.NA, pd.NaT, float('inf'), float('-inf')], 'N/A')
-        save_table(df, pdf_path.replace(".pdf",".xlsx").replace(INPUT_FOLDER,OUTPUT_FOLDER))
+        df.columns = [
+            "Name",
+            "DOB",
+            "DOD",
+            "Declarant",
+            "Data Of Notoriety",
+            "Notary",
+            "Address",
+            "Phone",
+            "Email",
+            "Website",
+            "Status",
+        ]
+        df = df.replace([pd.NA, pd.NaT, float("inf"), float("-inf")], "N/A")
+        save_table(
+            df, pdf_path.replace(".pdf", ".xlsx").replace(INPUT_FOLDER, OUTPUT_FOLDER)
+        )
 
         shutil.move(pdf_path, f"{COMPLETED_FOLDER}/{pdf}")
         print(f"\n\nTotal Cost : ${total_cost}")
-        print(f"Completed in {int(time.time() - ptime)} sec\nTotal : {int(time.time()-time_start)} sec")
-        
+        print(
+            f"Completed in {int(time.time() - ptime)} sec\nTotal : {int(time.time()-time_start)} sec"
+        )
+
+
 if __name__ == "__main__":
-    if is_tesseract_installed():
-        main()
-    else:
-        input("! ERROR !\n\ntesseract-fra is not Installed")
+    main()
--- src/image_processing.py
+++ src/image_processing.py
@@ -10,11 +10,14 @@ from functools import lru_cache
 from .utils import *
 
 
-openai_client = OpenAI(api_key = OPENAI_API_KEY)
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
+
 
 def get_image_result(image_path):
     text = pytesseract.image_to_string(image_path, lang="fra")
-    prompt = text + """
+    prompt = (
+        text
+        + """
 
 
     Please filter unnecessary characters like (*,#)
@@ -28,55 +31,65 @@ def get_image_result(image_path):
         "address": "",
     }
     """
-    response = openai_client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-        "role": "user",
-        "content": [
-            {"type": "text", "text": prompt},
-        ],
-        }
-    ],
-    response_format={ "type": "json_object" },
-    max_tokens=300,
     )
-    cost = (response.usage.prompt_tokens/1000) * 0.0005 + (response.usage.completion_tokens/1000) * 0.0015
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                ],
+            }
+        ],
+        response_format={"type": "json_object"},
+        max_tokens=300,
+    )
+    cost = (response.usage.prompt_tokens / 1000) * 0.0005 + (
+        response.usage.completion_tokens / 1000
+    ) * 0.0015
     result = eval(response.choices[0].message.content)
-        
+
     return result, cost
+
 
 @lru_cache(maxsize=None)
 def get_contact(name):
     try:
-        url = f'https://www.notaires.fr/en/directory/notaries?name={name}'
+        url = f"https://www.notaires.fr/en/directory/notaries?name={name}"
         response = requests.get(url)
         response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         phone = email = website = "Not Found"
-        
-        notary_page_element = soup.find('a', class_='arrow-link')
+
+        notary_page_element = soup.find("a", class_="arrow-link")
         if notary_page_element:
-            notary_page_href = notary_page_element['href']
+            notary_page_href = notary_page_element["href"]
             notary_page_link = "https://www.notaires.fr" + notary_page_href
-            
+
             response = requests.get(notary_page_link)
             response.raise_for_status()
-            soup = BeautifulSoup(response.content, 'html.parser')
-            
-            if response:            
-                phone_element = soup.find('div', class_='office-sheet__phone field--telephone')
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            if response:
+                phone_element = soup.find(
+                    "div", class_="office-sheet__phone field--telephone"
+                )
                 if phone_element:
                     phone = phone_element.text
-                    
-                email_button = soup.find('a', class_='btn-sheet btn-size--size-m btn-sheet--mail')
+
+                email_button = soup.find(
+                    "a", class_="btn-sheet btn-size--size-m btn-sheet--mail"
+                )
                 if email_button:
                     email = email_button["href"]
-                
-                website_element = soup.find('div', class_='office-sheet__url field--link')
+
+                website_element = soup.find(
+                    "div", class_="office-sheet__url field--link"
+                )
                 if website_element:
                     website = website_element.text
-                        
+
         return phone, email, website
     except requests.RequestException:
         print()
@@ -84,7 +97,7 @@ def get_contact(name):
 
 
 def process_image(image):
-    result = ["","","","","","","","","","",None]
+    result = None
     cost = 0
     try:
         t = time.time()
@@ -104,31 +117,61 @@ def process_image(image):
         else:
             phone = email = website = ""
             notary = "Not Found"
-                
-        
-        print(f"     {image} in {int(time.time()-t)} sec",end="\r")
-        result = [name, "-", "-", declarant, don, notary, address, phone, email, website,None]
+
+        print(f"     {image} in {int(time.time()-t)} sec", end="\r")
+        result = [
+            name,
+            "-",
+            "-",
+            declarant,
+            don,
+            notary,
+            address,
+            phone,
+            email,
+            website,
+            None,
+        ]
     except Exception as e:
         print(e)
-        
+
     return result, cost
 
-def is_tesseract_installed():
+
+def setup_tesseract():
     os_name = platform.system()
-    installed = False
     if os_name == "Windows":
-        if os.path.exists('C:/Program Files/Tesseract-OCR'):           
-            tesseract_path = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+        if os.path.exists("C:/Program Files/Tesseract-OCR"):
+            tesseract_path = "C:/Program Files/Tesseract-OCR/tesseract.exe"
             pytesseract.pytesseract.tesseract_cmd = tesseract_path
-            installed = True
+            if "fra" in pytesseract.get_languages():
+                return
+        else:
+            pass
+        print("!! tesseract is not installed !!")
+        print(
+            "Download and install tesseract : https://github.com/UB-Mannheim/tesseract/wiki"
+        )
+        print("Select French language during installation")
+        input()
+        sys.exit()
     else:
         try:
-            result = subprocess.run(['tesseract', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            result = subprocess.run(
+                ["tesseract", "--version"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
             if result.returncode == 0:
-                installed = True
+                if "fra" in pytesseract.get_languages():
+                    return
         except FileNotFoundError:
             pass
-        
-    if installed and "fra" in pytesseract.get_languages():
-        return True
-    return False
+        print("!! tesseract-fra is not installed !!")
+        print('Install tesseract-fra with this command : "brew install tesseract-fra"')
+        input()
+        sys.exit()
+
+
+setup_tesseract()
